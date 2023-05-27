75796b7161f842586832f210e222ab7c07b88df9
HRJHL <jinho99kr30@gmail.com>
2023-05-27 16:59:16 +0900

    teste
    11231

--- Assets/Scenes/TestScene/Drag.cs
+++ Assets/Scenes/TestScene/Drag.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class Drag : MonoBehaviour
 {
    float distance = 10;
+   GameObject BLLK;
 
     void OnMouseDrag()
     {
-        print("Drag!!");
+        
+        BLLK = GameObject.Find("BLLK");
         Vector3 mousePosition = new Vector3(Input.mousePosition.x, 
 Input.mousePosition.y, distance);
         Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        if(objPosition.y > 4){
+            objPosition.y = 4;
+        }
+        if(objPosition.y < -4){
+            objPosition.y = -4;
+        }
+        if(objPosition.x > 9){
+            objPosition.x = 8;
+        }
+        if(objPosition.x < BLLK.transform.position.x){
+            objPosition.x = BLLK.transform.position.x + 1;
+        }
         transform.position = objPosition;
     }
 
