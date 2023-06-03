d778af0aa5226c989c2801d0c156963231ada2f6
HRJHL <jinho99kr30@gmail.com>
2023-06-04 04:14:39 +0900

    11
    1

--- Assets/Scenes/StageScene1/Check.cs
+++ Assets/Scenes/StageScene1/Check.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Check : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    GameObject END;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+     public void OnClick(){
+           
     }
 }
